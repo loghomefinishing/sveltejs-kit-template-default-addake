6b9187d14e33390490a0ef44ca0cf89f80515d8a
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-10-13 20:16:15 -0600

    Initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,166 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+    </div>
+    <div class="flex flex-wrap">
+      <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
+        <p class="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <a class="text-indigo-500 inline-flex items-center">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">The Catalyzer</h2>
+        <p class="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <a class="text-indigo-500 inline-flex items-center">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Neptune</h2>
+        <p class="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <a class="text-indigo-500 inline-flex items-center">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Melanchole</h2>
+        <p class="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <a class="text-indigo-500 inline-flex items-center">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+    <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+  </div>
+</section>
+
+     <div class="flex justify-center  p-4">
+                                            <div class="flex flex-col justify-center items-center ">
+                                                <div class=" text-2xl md:text-5xl font-medium flex my-6">
+                                                    <div>Testimonials By </div>
+                                                    <div class="text-cyan-400 mx-1"> Trusted</div>
+                                                </div>
+                                                <div class="flex flex-col md:flex-row max-w-7xl justify-center items-center ">
+                                        
+                                                    <div class="overflow-hidden w-full m-4 flex justify-center   md:w-[33%]  shadow-xl bg-cyan-400">
+                                        
+                                                        <div class="flex flex-col md:flex-row items-center justify-center  bg-white rounded-tl-full ">
+                                                            <div class="  items-center justify-center flex py-2">
+                                                                <div class="flex flex-col  items-center justify-center ">
+                                                                    <div class="flex items-center">
+                                                                        <div class="p-1 bg-white rounded-full">
+                                        
+                                                                            <img src="https://source.unsplash.com/100x100/?man,boy" alt=""
+                                                                                class="rounded-full" />
+                                                                        </div>
+                                                                        <div>
+                                                                            <div class="font-bold text-stone-500 mx-4">John Doe</div>
+                                                                            <div class="text-sm font-medium text-stone-500 hover:text-stone-500 mx-4"><a
+                                                                                    href="#">Board Director of Tailblocks</a></div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="text-stone-400  m-2 px-8"> Tailblocks provides best Tailwind CSS Components and
+                                                                        Blocks to create an unique websites within minutes. It has upto 60+ free components for
+                                                                        front-end Web Development.</div>
+                                        
+                                                                </div>
+                                                            </div>
+                                        
+                                                        </div>
+                                                    </div>
+                                                    <div class="overflow-hidden w-full m-4 flex justify-center   md:w-[33%]  shadow-xl bg-cyan-400">
+                                        
+                                                        <div class="flex flex-col md:flex-row items-center justify-center  bg-white rounded-tl-full ">
+                                                            <div class="  items-center justify-center flex py-2">
+                                                                <div class="flex flex-col  items-center justify-center ">
+                                                                    <div class="flex items-center">
+                                                                        <div class="p-1 bg-white rounded-full">
+                                        
+                                                                            <img src="https://source.unsplash.com/100x100/?girl" alt="" class="rounded-full" />
+                                                                        </div>
+                                                                        <div>
+                                                                            <div class="font-bold text-stone-500 mx-4">Elina Gilbert</div>
+                                                                            <div class="text-sm font-medium text-stone-500 hover:text-stone-500 mx-4"><a
+                                                                                    href="#">ShareHolder, Tailblocks</a></div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="text-stone-400  m-2 px-8"> Tailblocks provides best Tailwind CSS Components and
+                                                                        Blocks to create an unique websites within minutes. It has upto 60+ free components for
+                                                                        front-end Web Development.</div>
+                                        
+                                                                </div>
+                                                            </div>
+                                        
+                                                        </div>
+                                                    </div>
+                                                    <div class="overflow-hidden w-full m-4 flex justify-center   md:w-[33%]  shadow-xl bg-cyan-400">
+                                        
+                                                        <div class="flex flex-col md:flex-row items-center justify-center  bg-white rounded-tl-full ">
+                                                            <div class="  items-center justify-center flex py-2">
+                                                                <div class="flex flex-col  items-center justify-center ">
+                                                                    <div class="flex items-center">
+                                                                        <div class="p-1 bg-white rounded-full">
+                                        
+                                                                            <img src="https://source.unsplash.com/100x100/?boy" alt="" class="rounded-full" />
+                                                                        </div>
+                                                                        <div>
+                                                                            <div class="font-bold text-stone-500 mx-4">Josh Reacher</div>
+                                                                            <div class="text-sm font-medium text-stone-500 hover:text-stone-500 mx-4"><a
+                                                                                    href="#">CEO, Tailblocks</a></div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="text-stone-400  m-2 px-8"> Tailblocks provides best Tailwind CSS Components and
+                                                                        Blocks to create an unique websites within minutes. It has upto 60+ free components for
+                                                                        front-end Web Development.</div>
+                                        
+                                                                </div>
+                                                            </div>
+                                        
+                                                        </div>
+                                                    </div>
+                                        
+                                        
+                                        
+                                                </div>
+                                            </div>
+                                        </div>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+	</p>
+
+	<pre>npm create svelte@latest</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+</div>
